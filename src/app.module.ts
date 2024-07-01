2da1e7445eb8f5a14e8bd0b0d43117a53800d58d
Ashrafu <ashraftuyubahe001@gmail.com>
2024-07-01 20:44:05 +0200

    starting making new api of user  management

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,28 @@
 import { Module } from '@nestjs/common';
 import { BooksController } from './books/books.controller';
 import { BookManipulateService } from './book-manipulate/book-manipulate.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123',
+      database: 'first_nest_app',
+      autoLoadEntities: true,
+      synchronize: true,
+    })
+  ],
   controllers: [BooksController],
    providers: [BookManipulateService],
+ 
+
 
 
 })
