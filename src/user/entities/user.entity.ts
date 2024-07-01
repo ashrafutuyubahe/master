b1c9f5074ff3679bb15bc134a34bb89be73ee8c8
Ashrafu <ashraftuyubahe001@gmail.com>
2024-07-01 21:14:29 +0200

    creating user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,6 +1,14 @@
-import { Repository, } from "typeorm";
-
-
+import {Column,PrimaryGeneratedColumn,Entity } from "typeorm";
+@Entity()
 export class User {
+@PrimaryGeneratedColumn()
+id:number;
+@Column({type:'varchar',length:30})
+userName:string;
+@Column({type:'varchar',length:30})
+userPassword:string;
+@Column({type:'varchar',length:30})
+userEmail:string;
+
  
 }
