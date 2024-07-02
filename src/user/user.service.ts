363394278e5778a855c14972ef356bb3d8f96ff5
Ashrafu <ashraftuyubahe001@gmail.com>
2024-07-02 21:32:46 +0200

    fixing new  bug

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,46 +4,47 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { Repository } from 'typeorm';
 import { users } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-
+import { EntityNotFoundException } from './exceptions/entity-not-found.exception';
 
 @Injectable()
 export class UserService {
-  constructor( @InjectRepository(users) private readonly   userRepository:Repository<users> ){
+  constructor(
+    @InjectRepository(users) private readonly userRepository: Repository<users>,
+  ) {}
+  create(CreateUserDto: CreateUserDto): Promise<users> {
+    const userInstance: users = new users();
+    userInstance.userName = CreateUserDto.userName;
+    userInstance.userPassword = CreateUserDto.userPassword;
+    userInstance.userEmail = CreateUserDto.userEmail;
+    userInstance.id = CreateUserDto.id;
 
-  }
-  create(CreateUserDto: CreateUserDto):Promise<users> {
-    const userInstance:users = new users();
-      userInstance.userName = CreateUserDto.userName;
-      userInstance.userPassword = CreateUserDto.userPassword;  
-      userInstance.userEmail = CreateUserDto.userEmail;
-      userInstance.id = CreateUserDto.id;
-
-
-  return  this.userRepository.save(userInstance);
-   
+    return this.userRepository.save(userInstance);
   }
 
   findAll() {
-  
-  return this.userRepository.find();
-
-
+    return this.userRepository.find();
   }
 
-  findOne(id: number) {
-    return `The  user  with this id is ${this.userRepository.findOneBy({id})}`;
+  async findOne(id: number) {
+    const findUser = await this.userRepository.findOneBy({ id });
+    return `The  user  with this id is ${findUser.userName}`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-        const updateInstance= new users;
-        updateInstance.userName= updateUserDto.userName;
-        updateInstance.userEmail= updateUserDto.userEmail;
-        updateInstance.userPassword= updateUserDto.userPassword
-       return this.userRepository.save(updateInstance);
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const existingUser = await this.userRepository.findOneBy({ id });
+    if (!existingUser) {
+      throw new EntityNotFoundException('the user is not found');
+    }
 
-  }
+    existingUser.userEmail = updateUserDto.userEmail;
+    existingUser.userName = updateUserDto.userName;
+    existingUser.userPassword = updateUserDto.userPassword;
 
-  remove(id: number) {
-   this.userRepository.delete(id);  
+    const saveUser = await this.userRepository.save(existingUser);
+    if (!saveUser) {
+      throw new EntityNotFoundException('failed to update user');
+    }
+
+    return 'user updated successfully';
   }
 }
