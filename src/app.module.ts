786715192617339930c755edfa3133b17aee2b58
ashraftuyubahe <ashrafutyubahe001u@gmail.com>
2024-12-17 09:18:51 +0200

    refactored folder structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { BooksController } from './new_src/books/books.controller';
 import { BookManipulateService } from './new_src/book-manipulate/book-manipulate.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './new_src/user/user.module';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './new_src/auth/auth.module';
 import { NewSrcModule } from './new_src/new_src.module';
 
 @Module({
